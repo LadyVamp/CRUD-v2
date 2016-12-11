1c273aac3f5e55b0d807d8cae6a90c380ca3682f
LadyVamp <ladyvamp666@gmail.com>
2016-12-12 00:35:50 +0200

    sql connection

--- Domain/File.cs
+++ Domain/File.cs
@@ -63,5 +63,36 @@ namespace Domain
             Content = content;
         }
 
+         //Соединение с БД
+         OleDbConnection connection;
+         OleDbCommand command;
+         private void ConnectTo()
+         {
+             connection = new OleDbConnection(@"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SearchBase;Data Source=NADYA-PC");
+             command = connection.CreateCommand();
+         }
+
+         private const string CONNECTION_STRING =
+    "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SearchBase;Data Source=NADYA-PC";
+
+        //Поиск по ключевым словам, формату и размеру файла
+        //ввести в checkbox и textbox, вывести в datagridview
+         public void Search(File f)
+         {
+             using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+             {
+                 con.Open();
+
+                 using (SqlCommand command = con.CreateCommand())
+                 {
+                     command.CommandType = System.Data.CommandType.Text;
+                     //command.CommandText = "INSERT INTO TSearchPattern (regularExpression, compareWith, action) VALUES('" + arsp.RegularExpression + "', '" + arsp.CompareWith + "', '" + arsp.Action + "')";
+                     //здесь сложный запрос
+                     //TODO. глянуть примеры https://www.youtube.com/results?search_query=datagridview+checkbox+c%23+
+                     command.ExecuteNonQuery();
+                 }
+             }
+
+         }
     }
 }
